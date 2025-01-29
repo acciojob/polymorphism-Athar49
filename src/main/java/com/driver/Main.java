56ac8add2b477b4080fbd5c2939c92bf2e88f901
SULTAN4342\ATHAR <mhasnainansari15@gmail.com>
2025-01-29 17:19:16 +0530

    Modified some values

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Product p = new Product();
         int res = p.product(10, 20);
         int res1 = p.product(5, 10);
-        double res2 = p.product(10.0, 20.0);
+        double res2 = p.product(10.5, 19.5);
 
         System.out.println(res + " " + res1 + " " + res2);
     }
